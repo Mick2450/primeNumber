82068f082c8a61a19bc2d99869df68385aa2f5b5
Mick <47966126+Mick2450@users.noreply.github.com>
2019-04-03 09:59:42 +1100

    primeNum main.c
    prime number function

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int isPrime(int x); //function prototype
+
+int main(){
+    int testNum;
+        
+    for(testNum=2; testNum<=100; testNum++){    //prime number is > 1 and cannot be formed by multiplying two smaller numbers
+        isPrime(testNum);   //testNum becomes x in function
+    }
+}
+
+
+int isPrime(int x){             //function definition 
+    int i, flag = 0, testNum;   // set flag = 0
+    for(i=2; i<=x/2; i++){      //if input value is cleanly divisible between 2 and input/2, it is not a prime number
+        if(x%i == 0){
+            flag = 1;           //flag becomes = 1 if number is not prime
+        }    
+        }
+        
+    if (flag == 0) {            //if prime (flag = 0) print 
+        printf("%d is a prime number\n", x); 
+    } 
+    else { 
+        return 0; 
+    }     
+    
+    
+    
+}
